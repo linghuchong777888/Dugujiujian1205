adb17a293c7c3a78340d3d88fee4bb20f4b7c7a8
linghuchong <linghuchong777888@126.com>
2019-05-31 15:33:19 +0800

    修改1

--- src/main/java/com/atguigu/jianfa1205/DugujiuajianApp.java
+++ src/main/java/com/atguigu/jianfa1205/DugujiuajianApp.java
@@ -3,6 +3,6 @@ package com.atguigu.jianfa1205;
 public class DugujiuajianApp {
 
     public static void main(String[] args) {
-        System.out.println("hello  dugujiujian ..........");
+        System.out.println("hello  dugujiujian ...------.......");
     }
 }

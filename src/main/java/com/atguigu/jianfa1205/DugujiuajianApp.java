e6bc4bb4fde40bef0e60fa48e4699115b8a3ec84
yuebuqun777888 <yuebuqun777888@126.com>
2019-05-31 15:39:44 +0800

    AAAAA

--- src/main/java/com/atguigu/jianfa1205/DugujiuajianApp.java
+++ src/main/java/com/atguigu/jianfa1205/DugujiuajianApp.java
@@ -4,5 +4,10 @@ public class DugujiuajianApp {
 
     public static void main(String[] args) {
         System.out.println("hello  dugujiujian ...------.......");
+        System.out.println("AAAAAAAAAAAAAAAAAAAA");
+        System.out.println("AAAAAAAAAAAAAAAAAAAA");
+        System.out.println("AAAAAAAAAAAAAAAAAAAA");
+        System.out.println("AAAAAAAAAAAAAAAAAAAA");
+        System.out.println("AAAAAAAAAAAAAAAAAAAA");
     }
 }
